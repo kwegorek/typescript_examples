2e416c30d970e3813005df930393c286880bcc07
kwegorek <50807299+kwegorek@users.noreply.github.com>
2020-06-12 20:38:45 +0200

    Validation

--- examples.ts
+++ examples.ts
@@ -543,7 +543,7 @@ class Person {
 
 
 ----------------------------------------------------------------------------------------------------------------------------
-BINDING - HANDING FORM SUBMITS
+# BINDING - HANDLING FORM SUBMITS
 
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -577,6 +577,105 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   }
 
 
+# VALIDATE TUPLE 
 
+private gatherUserInput(): [string, string, number] | void {
+  const enteredTitle = this.titleInput.value;
+  const enteredDescription = this.descriptionInput.value;
+  const enteredPeople = this.peopleInput.value;
+  // console.log('hello');
+
+  if (
+    enteredTitle.trim().length === 0
+    || enteredDescription.trim().length === 0
+    || enteredPeople.trim().length === 0
+  ) {
+    return;
+  }
+
+  // eslint-disable-next-line consistent-return
+  return [enteredTitle, enteredDescription, +enteredPeople];
+}
+
+
+# VALIDATION 
+
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {  //funtion geet the object with a strtructure
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null
+    && typeof validatableInput.value === 'string'
+  ) {
+    isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null
+    && typeof validatableInput.value === 'string'
+  ) {
+    isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null
+    && typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null
+    && typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
+
+private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInput.value;
+    const enteredDescription = this.descriptionInput.value;
+    const enteredPeople = this.peopleInput.value;
+    // console.log('hello');
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validate(titleValidatable)
+      || !validate(descriptionValidatable)
+      || !validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again!');
+      return;
+    }
+
+    // eslint-disable-next-line consistent-return
+    return [enteredTitle, enteredDescription, +enteredPeople];
+  }
 
 
